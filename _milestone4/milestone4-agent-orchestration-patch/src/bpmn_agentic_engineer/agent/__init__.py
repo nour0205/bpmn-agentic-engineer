0e2446c479fe441eca50f725e846c0d18a95ece8
NourK <nour.kouider@medtech.tn>
2026-08-17 14:40:24 +0100

    chore: clean public repository structure

--- _milestone4/milestone4-agent-orchestration-patch/src/bpmn_agentic_engineer/agent/__init__.py
+++
@@ -1,5 +0,0 @@
-from .graph import build_agent_graph
-from .service import AgentService
-from .state import AgentState, AgentStatus
-
-__all__ = ["AgentService", "AgentState", "AgentStatus", "build_agent_graph"]
